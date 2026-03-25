68bdcfc994beb5bcde35f5bcba382d30efbac943
madeincoder <ctgseafood@gmail.com>
2026-03-26 00:37:07 +0600

    header, footer and apps data added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./layout/Root.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Apps from "./pages/Apps/Apps.jsx";
+import Installation from "./pages/Installation/Installation.jsx";
+import ErrorPage from "./layout/NavBar/ErrorPage.jsx";
+import AppDetails from "./pages/Apps/AppDetails.jsx";
+import ContextAppComponent from "./contextApp/ContextAppComponent.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/apps",
+        element: <Apps></Apps>,
+      },
+      {
+        path: "/myinstallation",
+        element: <Installation></Installation>,
+      },
+      {
+        path: "/appDetails/:Id",
+        element: <AppDetails></AppDetails>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ContextAppComponent>
+      <RouterProvider router={router} />,
+    </ContextAppComponent>
   </StrictMode>,
-)
+);
